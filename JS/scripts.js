c93151f3ba0442c2f1b594b383125e5964994922
Galgallo Tadicha <tadichgalgallo@gmail.com>
2020-03-30 12:10:58 +0300

    add a validate form function to validate year of birth

--- JS/scripts.js
+++ JS/scripts.js
@@ -2,3 +2,10 @@ var d, CC, YY, MM, DD, dayValue;
 var dayNames = ["sunday","monday","tuesday","wednesday","thursday","friday","saturday" ];
 var femaleNames = ["Akosua","Adwoa","Abenaa","Akua"," Yaa","Afua","Ama"];
 var maleNames = ["Kwasi","Kwadwo","Kwabena","Kwaku","Yaw", "Kofi","Kwame"];
+
+function (validate) {
+    var gender= document.getElementsByName("gender")
+    if(document.myForm.year.value !=4 || document.myForm.year.value > 2020) {
+        alert("Please provise a valid year of birth!")
+    } else if(document.myform.month.vale !=2 || document.myForm.month.value >31 || document.myform.)
+}
