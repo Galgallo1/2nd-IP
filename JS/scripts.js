8a057156547ad422dc03c39b4eeb536101fad196
Galgallo Tadicha <tadichgalgallo@gmail.com>
2020-03-30 14:42:49 +0300

    add calculateDayValue function

--- JS/scripts.js
+++ JS/scripts.js
@@ -3,21 +3,13 @@ var dayNames = ["sunday","monday","tuesday","wednesday","thursday","friday","sat
 var femaleNames = ["Akosua","Adwoa","Abenaa","Akua"," Yaa","Afua","Ama"];
 var maleNames = ["Kwasi","Kwadwo","Kwabena","Kwaku","Yaw", "Kofi","Kwame"];
 
-function validate() {
-    var gender= document.getElementsByName("gender")
-    if(document.myForm.year.value !=4 || document.myForm.year.value > 2020) {
-        alert("Please provise a valid year of birth!");
-        document.myForm.year.value.focus();
-        return false;
-    } else if(document.myform.month.value !=2 || document.myForm.month.value >12 || document.myform.month.value <0){
-        alert("please provide a valid month of birth!");
-        document.myForm.month.value.focus();
-        return false;
-    } else if (document.myForm.date.value !=2 || document.myForm.date.value >31 || document.myForm.date.value <0) {
-        alert("Please provide a valid date!");
-        document.myForm.date.value.focuse();
-        return false
-    } else{
-        return true
-    }
-}
+function calculateDayValue(){
+    year = document.getElementById("year").value;
+    CC = parseInt(year.substring(0,2));
+    YY = parseInt(year.substring(2,4));
+    MM = parseInt(document.getElementById("month").value);
+    DD = parseInt(document.getElementById("date").value);
+    d = ( ( (CC/4) -2*CC-1) + ( (5*YY/4) ) + ((26*(MM+1)/10) ) + DD)%7;
+    console.log(d);
+    return (Math.floor(d));
+  }
