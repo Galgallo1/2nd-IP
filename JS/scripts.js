72b5e583350e5eb2f3fb57934bffe33bd31b35eb
Galgallo Tadicha <tadichgalgallo@gmail.com>
2020-03-30 10:17:27 +0300

    add dayNames, femalenames and malenames variable

---
+++ JS/scripts.js
@@ -0,0 +1,4 @@
+var d, CC, YY, MM, DD, dayValue;
+var dayNames = ["sunday","monday","tuesday","wednesday","thursday","friday","saturday" ];
+var femaleNames = ["Akosua","Adwoa","Abenaa","Akua"," Yaa","Afua","Ama"];
+var maleNames = ["Kwasi","Kwadwo","Kwabena","Kwaku","Yaw", "Kofi","Kwame"];
