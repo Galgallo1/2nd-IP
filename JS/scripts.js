edb2e37ea5caf383f8601823da86f0beacb36eab
Galgallo Tadicha <tadichgalgallo@gmail.com>
2020-03-30 12:20:48 +0300

    add a form validation function for date and month

--- JS/scripts.js
+++ JS/scripts.js
@@ -3,9 +3,21 @@ var dayNames = ["sunday","monday","tuesday","wednesday","thursday","friday","sat
 var femaleNames = ["Akosua","Adwoa","Abenaa","Akua"," Yaa","Afua","Ama"];
 var maleNames = ["Kwasi","Kwadwo","Kwabena","Kwaku","Yaw", "Kofi","Kwame"];
 
-function (validate) {
+function validate() {
     var gender= document.getElementsByName("gender")
     if(document.myForm.year.value !=4 || document.myForm.year.value > 2020) {
-        alert("Please provise a valid year of birth!")
-    } else if(document.myform.month.vale !=2 || document.myForm.month.value >31 || document.myform.)
+        alert("Please provise a valid year of birth!");
+        document.myForm.year.value.focus();
+        return false;
+    } else if(document.myform.month.value !=2 || document.myForm.month.value >12 || document.myform.month.value <0){
+        alert("please provide a valid month of birth!");
+        document.myForm.month.value.focus();
+        return false;
+    } else if (document.myForm.date.value !=2 || document.myForm.date.value >31 || document.myForm.date.value <0) {
+        alert("Please provide a valid date!");
+        document.myForm.date.value.focuse();
+        return false
+    } else{
+        return true
+    }
 }
